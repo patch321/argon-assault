0211812a8b9ba12c8ebb0c55d0984dba018bbf5f
Patrick <phartley@works24.com>
2018-03-10 21:42:43 -0600

    added music player

---
+++ Assets/musicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadNextScene", 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadNextScene() 
+    {
+        SceneManager.LoadScene(1);
+    }
+}
