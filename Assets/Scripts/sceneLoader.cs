23d9b7a8110a5a1d98cdfed10a739cdbd5293208
Patrick Hartley <phartley@works24.com>
2018-03-14 00:16:21 -0500

    add box collider and rigidbody to ship

---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+    {
+        Invoke("LoadNextScene", 3f);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
