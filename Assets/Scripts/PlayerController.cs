0257ae5d9368d7d36b3168b27893e42febd145d1
Patrick Hartley <phartley@works24.com>
2018-03-15 19:44:19 -0500

    adding trace and collider detection

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("General")]
+    [SerializeField][Tooltip("In meters/second")] float controlSpeed = 15f;
+    [SerializeField] [Tooltip("In meters")] float xRange = 5f;
+    [SerializeField] [Tooltip("In meters")] float yRange = 3f;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = -3f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float controlPitchFactor = -5f;
+    [SerializeField] float controlRollFactor = -3f;
+
+    float startingXPostition;
+    float startingYPostition;
+    float xThrow, yThrow;
+
+    // Use this for initialization
+    void Start () {
+        this.startingXPostition = this.transform.position.x;
+        this.startingYPostition = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPostion = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToControlThrow + pitchDueToPostion;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float newXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+        float newYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, transform.localPosition.z);
+    }
+}
--- Assets/Scripts/musicPlayer.cs
+++ Assets/Scripts/musicPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class musicPlayer : MonoBehaviour {
+public class MusicPlayer : MonoBehaviour {
 
     // Use this for initialization
     void Awake()
