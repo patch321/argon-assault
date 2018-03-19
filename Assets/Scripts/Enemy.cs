8bf6ddafff9dc05576002a713aabd4401be40781
unknown <patrickhartleyjr@gmail.com>
2018-03-18 23:45:50 -0500

    add enemy explosions and particle colliders

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+	// Use this for initialization
+	void Start () {
+        AddNonTriggerBoxCollider();
+	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
